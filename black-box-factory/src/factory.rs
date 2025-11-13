a686b14c2ee985248ed61895f9ae5abae1e06431
Caleb Leinz <cleinz+github@pm.me>
2025-11-12 21:31:31 -0800

    feat: Implement actor for Overseer, add messages for adding/removing resources

--- black-box-factory/src/factory.rs
+++ black-box-factory/src/factory.rs
@@ -1,12 +1,76 @@
+use black_box::{Actor, Handler};
 use std::any::{Any, TypeId};
+use std::marker::PhantomData;
 
-use crate::factory_set::FactorySet;
 use crate::Factory;
+use crate::factory_set::FactorySet;
+
+pub struct RemoveResource<U>(PhantomData<U>);
+
+impl<U> Default for RemoveResource<U> {
+    fn default() -> Self {
+        Self(PhantomData)
+    }
+}
+
+impl<U> RemoveResource<U> {
+    pub fn new() -> Self {
+        Self::default()
+    }
+}
+
+pub struct InsertResource<U>(U);
+
+impl<U> InsertResource<U> {
+    pub fn new(value: U) -> Self {
+        Self(value)
+    }
+}
+
+pub struct UpdateResource<U>(U);
+
+impl<U> UpdateResource<U> {
+    pub fn new(value: U) -> Self {
+        Self(value)
+    }
+}
 
 pub struct Overseer<T> {
     map: crate::ResourcePool,
     factory_set: FactorySet<T>,
     handles: Vec<T>,
+}
+
+impl<T> Actor for Overseer<T> {}
+
+impl<T, U> Handler<RemoveResource<U>> for Overseer<T>
+where
+    U: Any + Send + Sync,
+    T: Send,
+{
+    async fn handle(&mut self, _msg: RemoveResource<U>, _ctx: &black_box::Context<Self>) {
+        self.remove_resource::<U>();
+    }
+}
+
+impl<T, U> Handler<InsertResource<U>> for Overseer<T>
+where
+    U: Any + Send + Sync,
+    T: Send,
+{
+    async fn handle(&mut self, msg: InsertResource<U>, _ctx: &black_box::Context<Self>) {
+        self.insert_resource(msg.0);
+    }
+}
+
+impl<T, U> Handler<UpdateResource<U>> for Overseer<T>
+where
+    U: Any + Send + Sync,
+    T: Send,
+{
+    async fn handle(&mut self, msg: UpdateResource<U>, _ctx: &black_box::Context<Self>) {
+        self.update_resource(msg.0);
+    }
 }
 
 impl<T> Default for Overseer<T> {
