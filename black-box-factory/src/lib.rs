390698d77d04dd3e17c9210f927e428d013014a5
Caleb Leinz <cleinz+github@pm.me>
2025-11-13 13:58:39 -0800

    feat: Add ActorHandle wrapper around ShutdownHandle

--- black-box-factory/src/lib.rs
+++ black-box-factory/src/lib.rs
@@ -8,6 +8,27 @@ pub trait Handle {
     fn shutdown(&mut self, pool: &ResourcePool);
 }
 
+#[derive(Debug, Clone)]
+pub struct ActorHandle(black_box::ShutdownHandle);
+
+impl ActorHandle {
+    pub fn new(value: black_box::ShutdownHandle) -> Self {
+        Self::from(value)
+    }
+}
+
+impl From<black_box::ShutdownHandle> for ActorHandle {
+    fn from(value: black_box::ShutdownHandle) -> Self {
+        Self(value)
+    }
+}
+
+impl Handle for ActorHandle {
+    fn shutdown(&mut self, _pool: &ResourcePool) {
+        let _ = self.0.shutdown();
+    }
+}
+
 #[derive(Debug, Clone, Copy, PartialEq, Eq, PartialOrd, Ord)]
 #[non_exhaustive]
 pub enum Action {
